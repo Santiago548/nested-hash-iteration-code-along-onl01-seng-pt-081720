d35addedc647d040de22f772d84fd85b3e7472d4
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 23:18:43 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |name, data|
-    if person == "Freddy Mercury"
+    if name == "Freddy Mercury"
     data.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
